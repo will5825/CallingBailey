50016305e37ab95ec23504e5b2d3a57af4d90515
William Tran <tranwt@mcmaster.ca>
2016-10-22 18:25:12 -0400

    Added stuff to classes

--- Source/CallingBailey/src/Query.java
+++ Source/CallingBailey/src/Query.java
@@ -1,9 +1,33 @@
 
 public class Query {
 	
-	String id;
-	QueryType type;
-	QueryWhen when;
+	final String queryId;
+	final String callerId;
+	final QueryType type;
+	final QueryWhen when;
+	
+	public Query(String queryId, String callerId, QueryType type, QueryWhen when) {
+		this.queryId = queryId;
+		this.callerId = callerId;
+		this.type = type;
+		this.when = when;
+	}
+
+	public String getQueryId() {
+		return queryId;
+	}
+
+	public String getCallerId() {
+		return callerId;
+	}
+
+	public QueryType getType() {
+		return type;
+	}
+
+	public QueryWhen getWhen() {
+		return when;
+	}
 	
 	
 }
--- Source/CallingBailey/src/Receiver.java
+++ Source/CallingBailey/src/Receiver.java
@@ -1,4 +1,21 @@
+import java.util.Queue;
+
 
 public class Receiver {
-
+	
+	private Queue<Query> queryQueue;
+	
+	public Receiver(){
+		
+	}
+	
+	public void addQuery(Query q){ 
+		queryQueue.add(q);
+	}
+	
+	public Query getQuery(){
+		if (!queryQueue.isEmpty())
+			return queryQueue.remove();
+		return null;
+	}
 }
