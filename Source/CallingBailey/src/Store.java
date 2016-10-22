81809f5e9f200f5b67522941f7e1f75d7b547f72
Susan Yuen <susan_loves_cheese@hotmail.com>
2016-10-22 18:47:09 -0400

    Finished Store class

--- Source/CallingBailey/src/Store.java
+++ Source/CallingBailey/src/Store.java
@@ -1,4 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Store {
 
+	public void storeErrorLog(String error) {
+		writeToFile(error, "ErrorLog.txt");
+	}
+
+	public void storeWeatherLog(String weather) {
+		writeToFile(weather, "WeatherLog.txt");
+	}
+
+	public void storeWarningLog(String warning) {
+		writeToFile(warning, "WarningLog.txt");
+	}
+
+	public void storeOceanCurrentLog(String current) {
+		writeToFile(current, "OceanCurrentLog.txt");
+	}
+
+	public void storeUserId(String id) {
+		writeToFile(id, "UserIDs.txt");
+	}
+
+	private void writeToFile(String str, String filePath) {
+		try {
+			File file = new File("/Logs/" + filePath);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(str);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
